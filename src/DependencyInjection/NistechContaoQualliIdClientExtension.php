60856d0d1e33cd8d9cacf20f19921f329709860a
Sylvio <spielhagen@nistech.de>
2025-05-29 14:34:51 +0200

    Korrektur

--- src/DependencyInjection/NistechContaoQualliIdClientExtension.php
+++ src/DependencyInjection/NistechContaoQualliIdClientExtension.php
@@ -2,16 +2,6 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of Contao OAuth2 Client.
- *
- * (c) Marko Cupic <m.cupic@gmx.ch>
- * @license GPL-3.0-or-later
- * For the full copyright and license information,
- * please view the LICENSE file that was distributed with this source code.
- * @link https://github.com/markocupic/contao-oauth2-client
- */
-
 namespace Nistech\ContaoQualliIdClient\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
@@ -19,7 +9,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class MarkocupicContaoOAuth2ClientExtension extends Extension
+class NistechContaoQualliIdClientExtension extends Extension
 {
     /**
      * {@inheritdoc}
